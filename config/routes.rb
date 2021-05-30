3d6136f1d035bae25149be8ba2a98adfeea95e3b
tani <tani@1-10.com>
2021-05-30 11:28:00 +0900

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
-  get "post/1" => "posts#show"
-  get "post/2" => "posts#show"
-  get "post/3" => "posts#show"
+  get "post/index" => "posts#index"
+  get "post/:id" => "posts#show"
 end
