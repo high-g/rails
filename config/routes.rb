372c7ff8b42e5f9087902a645e9c4d5b71c5043b
tani <tani@1-10.com>
2021-05-30 11:25:27 +0900

    Update posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
+  get "post/1" => "posts#show"
+  get "post/2" => "posts#show"
+  get "post/3" => "posts#show"
 end
